d7167cf3676f56dbaa948261b649f8843571cf6e
Júlia Botelho <cajujumo@gmail.com>
2022-08-22 09:47:16 -0300

    versao-com-distribuicao-aleatoria-do-numero-correto-de-cartas

---
+++ script.js
@@ -0,0 +1,61 @@
+
+const cartas = document.querySelector('.content');
+let qtde;
+let num;
+function startgame(){
+    qtde = Number(prompt('Olá! Com quantas cartas prefere jogar?🃏'));
+    while (qtde < 4 || qtde > 14 || qtde % 2 !== 0){
+        qtde = Number(prompt('Perdão, não é possível jogar com esse número de cartas!🥺'));
+    }
+    if (qtde === 4) {
+        let versos4 = ['verso1','verso1','verso2','verso2'];
+        for(let c=0 ; c < qtde ; c++){
+            let num = Math.floor(Math.random() * versos4.length);
+            cartas.innerHTML = cartas.innerHTML + `<div onclick="virar(this)" class="card ${versos4[num]} frente"></div>`;
+            versos4.splice(num ,1);
+        }
+    }
+    if (qtde === 6) {   
+        let versos6 = ['verso1','verso1','verso2','verso2','verso3','verso3'];
+        for(let c=0 ; c < qtde ; c++){
+            let num = Math.floor(Math.random() * versos6.length);
+            cartas.innerHTML = cartas.innerHTML + `<div onclick="virar(this)" class="card ${versos6[num]} frente"></div>`;
+            versos6.splice(num ,1);
+        }
+    }
+    if (qtde === 8) {   
+        let versos8 = ['verso1','verso1','verso2','verso2','verso3','verso3','verso4','verso4'];
+        for(let c=0 ; c < qtde ; c++){
+            let num = Math.floor(Math.random() * versos8.length);
+            cartas.innerHTML = cartas.innerHTML + `<div onclick="virar(this)" class="card ${versos8[num]} frente"></div>`;
+            versos8.splice(num ,1);
+        }
+    }
+    if (qtde === 10) {   
+        let versos10 = ['verso1','verso1','verso2','verso2','verso3','verso3','verso4','verso4','verso5','verso5'];
+        for(let c=0 ; c < qtde ; c++){
+            let num = Math.floor(Math.random() * versos10.length);
+            cartas.innerHTML = cartas.innerHTML + `<div onclick="virar(this)" class="card ${versos10[num]} frente"></div>`;
+            versos10.splice(num ,1);
+        }
+    }
+    if (qtde === 12) {   
+        let versos12 = ['verso1','verso1','verso2','verso2','verso3','verso3','verso4','verso4','verso5','verso5','verso6','verso6'];
+        for(let c=0 ; c < qtde ; c++){
+            let num = Math.floor(Math.random() * versos12.length);
+            cartas.innerHTML = cartas.innerHTML + `<div onclick="virar(this)" class="card ${versos12[num]} frente"></div>`;
+            versos12.splice(num ,1);
+        }
+    }
+    if (qtde === 14) {   
+        let versos14 = ['verso1','verso1','verso2','verso2','verso3','verso3','verso4','verso4','verso5','verso5','verso6','verso6','verso7','verso7'];
+        for(let c=0 ; c < qtde ; c++){
+            let num = Math.floor(Math.random() * versos14.length);
+            cartas.innerHTML = cartas.innerHTML + `<div onclick="virar(this)" class="card ${versos14[num]} frente"></div>`;
+            versos14.splice(num ,1);
+        }
+    }
+}
+
+startgame();
+
